26a2274d9065dcdd2b22dbc35df6371df9e4190c
Harshita-sinha-cloud <ms.harshita.sinha@gmail.com>
2024-12-02 23:33:17 -0500

    Update actors_history_scd.sql

--- Dimension Data Modeling/actors_history_scd.sql
+++ Dimension Data Modeling/actors_history_scd.sql
@@ -1,10 +1,9 @@
 create table actors_history_scd(
 actor text,
-actorid text,
 quality_class quality_class,
-start_date integer,
-end_date integer,
+start_year integer,
+end_year integer,
 current_year integer,
 is_active bool,
-PRIMARY KEY (actor, start_date) 
+PRIMARY KEY (actor, start_year, end_year) 
 );
